06f0ba587e7afda25b32f341cac38b57f38aa05a
Artemy <tatyapa0880@yandex.ru>
2022-05-12 02:02:30 +0500

    urls in news

--- news/views.py
+++ news/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello world!</h1>')
+
+
+def test(request):
+    return HttpResponse('Test here!')

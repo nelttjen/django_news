202d714792368f2b644fd761e1a4bd78581c2b5c
Artemy <tatyapa0880@yandex.ru>
2022-05-12 01:58:43 +0500

    hw news

--- news_django/urls.py
+++ news_django/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from news.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('index/', index),
 ]

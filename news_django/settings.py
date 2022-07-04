4561666a42fe7834d1e73c3a67ac098503a2221c
Artemy <tatyapa0880@yandex.ru>
2022-07-04 22:09:22 +0500

    migrated to PostgreSQL

--- news_django/settings.py
+++ news_django/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'news_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
 
